df06ecad16dab2d9182d0b9e5ccb03b9a7fd1339
Johnny Fang <fangdejavu@gmail.com>
2019-03-02 12:24:38 -0500

    addded new css to ContactData added dynamic creation of inputs based on JS config added orderForm

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,58 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component{
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: '',
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-mail'
+                },
+                value: ''
+            },
+            deliverMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'regular', displayValue: 'Regular'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
@@ -24,16 +71,7 @@ class ContactData extends Component{
         const order = {
             ingredients: this.props.ingredients, 
             price: this.props.price,
-            customer: {
-                name: 'Johnny Fang',
-                address: {
-                    street: 'Elm Street',
-                    zipCode: '41351',
-                    country: 'Colombia'
-                },
-                email: 'test@test.com'
-            },
-            deliverMethod: 'fastest'
+            
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -48,12 +86,23 @@ class ContactData extends Component{
     }
 
     render () {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
         let form = (
             <form>
-                <Input inputType="input" type="email" name="email" placeholder="Your email"/>
-                <Input inputType="input" type="text" name="name" placeholder="Your Name"/>
-                <Input inputType="input" type="text" name="street" placeholder="Your street"/>
-                <Input inputType="input" type="text" name="postalCode" placeholder="postal Code"/>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id} 
+                        elementType={formElement.config.elementType} 
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button> 
             </form>
         );
